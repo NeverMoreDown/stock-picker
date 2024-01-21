408621042115e6f2237733d1e8029f70e1c0731c
Jakob Paddy <jakobpaddy@gmail.com>
2024-01-21 08:34:02 -0700

    add comments to code

--- stock-picker.rb
+++ stock-picker.rb
@@ -3,9 +3,14 @@ def stock_picker(prices)
     buy_day = 0
     sell_day = 0
   
+    # iterate through stock-prices array
     (0...prices.length).each do |i|
+        #iterate through stock prices array from positon of i + 1 till the end of prices array
       (i + 1...prices.length).each do |j|
+        #create profit as current j - i
         profit = prices[j] - prices[i]
+        #check to see with all iterations if current j - i is greater than max_profit 
+        #if so max_profit is now equal to profit with buy day equal to j and i 
         if profit > max_profit
           max_profit = profit
           buy_day = i
@@ -13,6 +18,6 @@ def stock_picker(prices)
         end
       end
     end
-  
+    #return the buy_day,sell_day
     return [buy_day, sell_day]
 end
